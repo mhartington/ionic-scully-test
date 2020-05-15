4e9b47aea495262c93a1300bafadba1142255a84
Mike Hartington <mikehartington@gmail.com>
2020-05-15 09:40:25 -0400

    add scully

---
+++ scully.app.config.ts
@@ -0,0 +1,9 @@
+import { ScullyConfig } from '@scullyio/scully';
+
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "app",
+  outDir: './dist/static',
+  routes: {
+  }
+};
